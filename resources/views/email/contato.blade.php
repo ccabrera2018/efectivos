5f755918992f25f7b02929ab8f468f2f1b5702c7
Hilario Bautista Miguel <hbautista@ape.net.mx>
2018-10-22 18:59:13 -0500

    cambio ejemplo

--- resources/views/email/contato.blade.php
+++ resources/views/email/contato.blade.php
@@ -8,7 +8,7 @@
 	<p><strong>{!! $name  !!}</strong></p>
 	<p><strong>{!! $telefono  !!}</strong></p>
 	<p><strong>{!! $email  !!}</strong></p>
-	<p><strong>GRACIAS POR CONFIAR EN NOSOSTROS</strong></p>
+	<p><strong>GRACIAS POR CONFIAR EN NOSOSTROS #</strong></p>
 
 </body>
 </html>
