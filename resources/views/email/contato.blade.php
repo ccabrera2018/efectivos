44290ce7fc7271e350f60770c096099fb607bdc5
Hilario Bautista Miguel <hbautista@ape.net.mx>
2018-10-22 19:24:24 -0500

    cambie EL CLIENTE DE CORREO

--- resources/views/email/contato.blade.php
+++ resources/views/email/contato.blade.php
@@ -9,6 +9,7 @@
 	<p><strong>{!! $telefono  !!}</strong></p>
 	<p><strong>{!! $email  !!}</strong></p>
 	<p><strong>GRACIAS POR CONFIAR EN NOSOSTROS #</strong></p>
+	<p><strong>CAMBIE LA SALIDA DEL TITULO</strong></p>
 
 </body>
 </html>
